d8f42937671dc784c77d2146db314066aa5c26ef
NaurahKamilaSyadza <naurahkamila26@gmail.com>
2023-12-04 23:32:10 +0700

    Modul5

---
+++ MODUL5-Naurah/app/Http/Controllers/ShowroomController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ShowroomController extends Controller
+{public function create (){
+    return view('inventory.addInventory');
+}
+public function store (request $request);{}
+    $this -> validate request
+    f
+ }
+    //
+}
